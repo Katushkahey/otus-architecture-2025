4cd2ac1e75712af534e741b6e7fcc77241c2ed1b
EkaIgIvanova <EkaIgIvanova@spb.beeline.ru>
2025-11-06 00:54:01 +0300

    Homework 2 fix health check path

--- src/main/java/com/otus/homework/HealthController.java
+++ src/main/java/com/otus/homework/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health/")
+    @GetMapping("/health")
     ResponseEntity<ResponseDto> health() {
         return ResponseEntity.ok(new ResponseDto("OK"));
     }
